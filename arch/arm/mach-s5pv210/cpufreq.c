29c54f4974f060313395315107895bba78c3b484
stratosk <stratosk@albert.(none)>
2012-01-26 00:29:30 +0200

    Transition latency 10000

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -626,7 +626,7 @@ static int __init s5pv210_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(s5pv210_freq_table, policy->cpu);
 
-	policy->cpuinfo.transition_latency = 40000;
+	policy->cpuinfo.transition_latency = 10000;
 
 #ifdef CONFIG_DVFS_LIMIT
 	int i;
