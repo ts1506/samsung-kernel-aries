12f5bd55c7d29fdda0aaafadf8d3df589be62d19
stratosk <stratosk@albert.(none)>
2012-06-01 16:32:44 +0300

    Typo in copyright

--- arch/arm/mach-s5pv210/bigmem.c
+++ arch/arm/mach-s5pv210/bigmem.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)  20012 stratosk <stratosk@semaphore.gr>
+ * Copyright (C)  2012 stratosk <stratosk@semaphore.gr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
