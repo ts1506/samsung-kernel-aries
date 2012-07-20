4bae152b0abe7912fb997867f00ea05efbaf081b
stratosk <stratosk@albert.(none)>
2012-07-21 01:13:33 +0300

    Merge remote-tracking branch 'google/android-samsung-3.0-jb' into HEAD

--- arch/s390/kernel/ptrace.c
+++ arch/s390/kernel/ptrace.c
@@ -20,8 +20,8 @@
 #include <linux/regset.h>
 #include <linux/tracehook.h>
 #include <linux/seccomp.h>
+#include <linux/compat.h>
 #include <trace/syscall.h>
-#include <asm/compat.h>
 #include <asm/segment.h>
 #include <asm/page.h>
 #include <asm/pgtable.h>
@@ -905,6 +905,14 @@ static int s390_last_break_set(struct task_struct *target,
 	return 0;
 }
 
+static int s390_last_break_set(struct task_struct *target,
+			       const struct user_regset *regset,
+			       unsigned int pos, unsigned int count,
+			       const void *kbuf, const void __user *ubuf)
+{
+	return 0;
+}
+
 #endif
 
 static const struct user_regset s390_regsets[] = {
@@ -1086,6 +1094,14 @@ static int s390_compat_last_break_get(struct task_struct *target,
 				return -EFAULT;
 		}
 	}
+	return 0;
+}
+
+static int s390_compat_last_break_set(struct task_struct *target,
+				      const struct user_regset *regset,
+				      unsigned int pos, unsigned int count,
+				      const void *kbuf, const void __user *ubuf)
+{
 	return 0;
 }
 
