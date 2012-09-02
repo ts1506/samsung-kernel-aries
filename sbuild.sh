2ee6ecee516e8f7ce9247918040ff39ff88d5ae2
stratosk <stratosk@semaphore.gr>
2012-09-02 12:06:04 +0300

    New build script

--- sbuild.sh
+++ sbuild.sh
@@ -1,26 +1,82 @@
 #!/bin/bash
 
-sema_ver="Semaphore_JB_2.0.6"
+BASE_SEMA_VER="Semaphore_JB_2.0.8"
+
+case "$1" in
+        galaxys)
+            VARIANT="galaxys"
+            VER=""
+            ;;
+
+        captivate)
+            VARIANT="captivate"
+            VER="c"
+            ;;
+
+        vibrant)
+            VARIANT="vibrant"
+            VER="v"
+            ;;
+
+        *)
+            VARIANT="galaxys"
+            VER=""
+esac
+
+if [ "$2" = "s" ] ; then
+	BASE_SEMA_VER=$BASE_SEMA_VER"s"
+fi
+
+SEMA_VER=$BASE_SEMA_VER$VER
 
 #export KBUILD_BUILD_VERSION="2"
-export LOCALVERSION="-"`echo $sema_ver`
+export LOCALVERSION="-"`echo $SEMA_VER`
+#export CROSS_COMPILE=/opt/toolchains/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/opt/toolchains/gcc-linaro-arm-linux-gnueabihf-2012.08-20120827_linux/bin/arm-linux-gnueabihf-
+export ARCH=arm
 
-make CROSS_COMPILE=/opt/toolchains/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf- ARCH=arm -j4 modules
+eval $(grep CONFIG_INITRAMFS_SOURCE .config)
+INIT_DIR=$CONFIG_INITRAMFS_SOURCE
+MODULES_DIR=`echo $INIT_DIR`files/modules
+KERNEL_DIR=`pwd`
+OUTPUT_DIR=/kernels/output/
+CWM_DIR=/kernels/ics-ramdisk/cwm/
 
-find /kernels/samsung-kernel-aries/ -name '*.ko' -exec cp -v {} /kernels/ics-ramdisk/jb_combo/files/modules \;
+echo "LOCALVERSION="$LOCALVERSION
+echo "CROSS_COMPILE="$CROSS_COMPILE
+echo "ARCH="$ARCH
+echo "INIT_DIR="$INIT_DIR
+echo "MODULES_DIR="$MODULES_DIR
+echo "KERNEL_DIR="$KERNEL_DIR
+echo "OUTPUT_DIR="$OUTPUT_DIR
+echo "CWM_DIR="$CWM_DIR
 
-make CROSS_COMPILE=/opt/toolchains/gcc-linaro-arm-linux-gnueabihf-2012.07-20120720_linux/bin/arm-linux-gnueabihf- ARCH=arm -j4 zImage
+echo 
+echo "Making ""semaphore"_$VARIANT"_defconfig"
+
+make "semaphore"_$VARIANT"_defconfig"
+
+if [ "$2" = "s" ] ; then
+        echo "CONFIG_S5P_HUGEMEM=y" >> .config
+fi
+
+
+make -j4 modules
+
+rm `echo $MODULES_DIR"/*"`
+find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+
+make -j4 zImage
 
 cd arch/arm/boot
-tar cvf `echo $sema_ver`.tar zImage
+tar cvf `echo $SEMA_VER`.tar zImage
+mv `echo $SEMA_VER`.tar $OUTPUT_DIR$VARIANT
+echo "Moving to "$OUTPUT_DIR$VARIANT"/"
 cd ../../../
 
-cp arch/arm/boot/zImage ../ics-ramdisk/cwm/boot.img
-
-cd ../ics-ramdisk/cwm
-
-zip -r `echo $sema_ver`.zip *
-
-mv  `echo $sema_ver`.zip ../
+cp arch/arm/boot/zImage $CWM_DIR"boot.img"
+cd $CWM_DIR
+zip -r `echo $SEMA_VER`.zip *
+mv  `echo $SEMA_VER`.zip $OUTPUT_DIR$VARIANT"/"
 
 
