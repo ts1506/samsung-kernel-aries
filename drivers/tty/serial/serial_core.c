2f874755745edc17df539d4b6f5dce82d3c16ba0
ts1506 <guharayshaunak75@gmail.com>
2013-05-09 23:39:14 +0530

    Fixed a small typo

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -1920,7 +1920,7 @@ int uart_suspend_port(struct uart_driver *drv, struct uart_port *uport)
 		mutex_unlock(&port->mutex);
 		return 0;
 	}
-	put_device(tty_dev)
+	put_device(tty_dev);
 
 	if (console_suspend_enabled || !uart_console(uport))
 		uport->suspended = 1;
