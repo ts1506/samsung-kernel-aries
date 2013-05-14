88fcd394178b65f68926e1459bcd78c96ee8eb4f
ts1506 <guharayshaunak75@gmail.com>
2013-05-14 07:59:33 +0530

    Semaphore AutoBrightness: Change to reference code

--- drivers/video/samsung/sema_autobr.c
+++ drivers/video/samsung/sema_autobr.c
@@ -171,7 +171,7 @@ static ssize_t store_max_lux(struct device *dev, struct device_attribute *attr,
 	int ret;
 
 	ret = sscanf(buf, "%hu", &input);
-	if (ret != 1 || input < 1 || input > (DRV_MAX_LUX + 3000))
+	if (ret != 1 || input < 0 || input > (DRV_MAX_LUX * 2))
 		return -EINVAL;
 
 	sa_tuners.max_lux = input;
