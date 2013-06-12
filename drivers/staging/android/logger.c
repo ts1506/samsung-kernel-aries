d7dab0b7f8a59d544cc4e2864ae776090a42a291
Shaunak Guharay <guharayshaunak75@gmail.com>
2013-06-12 15:08:30 +0530

    Reduce logger RAM allocation

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -806,19 +806,19 @@ static int __init logger_init(void)
 {
 	int ret;
 
-	ret = create_log(LOGGER_LOG_MAIN, 256*1024);
+	ret = create_log(LOGGER_LOG_MAIN, 32*1024);
 	if (unlikely(ret))
 		goto out;
 
-	ret = create_log(LOGGER_LOG_EVENTS, 256*1024);
+	ret = create_log(LOGGER_LOG_EVENTS, 32*1024);
 	if (unlikely(ret))
 		goto out;
 
-	ret = create_log(LOGGER_LOG_RADIO, 256*1024);
+	ret = create_log(LOGGER_LOG_RADIO, 32*1024);
 	if (unlikely(ret))
 		goto out;
 
-	ret = create_log(LOGGER_LOG_SYSTEM, 256*1024);
+	ret = create_log(LOGGER_LOG_SYSTEM, 32*1024);
 	if (unlikely(ret))
 		goto out;
 
