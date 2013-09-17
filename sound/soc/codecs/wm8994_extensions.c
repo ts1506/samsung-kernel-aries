e0005259b89ad4a9b60ce7d793a72ce233fd1904
Shaunak Guharay <guharayshaunak75@gmail.com>
2013-09-17 19:41:31 +0530

    Disable in-call volume settings

--- sound/soc/codecs/wm8994_extensions.c
+++ sound/soc/codecs/wm8994_extensions.c
@@ -90,7 +90,7 @@ unsigned int eq_band_values[5][4] = {
 bool stereo_expansion = false;
 short unsigned int stereo_expansion_gain = 16;
 
-#if 1
+#ifdef CONFIG_PHONE_ARIES_CDMA
 // call volume boost hack
 extern unsigned short incall_boost_rcv;
 extern unsigned short incall_boost_bt;
@@ -1074,7 +1074,7 @@ DECLARE_BOOL_STORE_UPDATE_WITH_MUTE(dac_direct,
 				    update_dac_direct,
 				    false);
 
-#if 1
+#ifdef CONFIG_PHONE_ARIES_CDMA
 static ssize_t incall_boost_rcv_show(struct device* dev, struct device_attribute* attr,
 				     char* buf)
 {
@@ -1685,7 +1685,7 @@ static DEVICE_ATTR(recording_preset, S_IRUGO | S_IWUGO,
 		   recording_preset_store);
 #endif
 
-#if 1
+#ifdef CONFIG_PHONE_ARIES_CDMA
 static DEVICE_ATTR(incall_boost_rcv, S_IRUGO | S_IWUGO,
 		   incall_boost_rcv_show,
 		   incall_boost_rcv_store);
@@ -1816,7 +1816,7 @@ static struct attribute *wm8994_extensions_attributes[] = {
 	&dev_attr_recording_preset.attr,
 #endif
 
-#if 1
+#ifdef CONFIG_PHONE_ARIES_CDMA
 	&dev_attr_incall_boost_rcv.attr,
 	&dev_attr_incall_boost_bt.attr,
 	&dev_attr_incall_boost_spk.attr,
